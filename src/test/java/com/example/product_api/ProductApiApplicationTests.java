9bafa79b9b58e4b20f8fdfc49fa2411228ee1f56
calcium4008 <calcium40088@gmail.com>
2026-08-20 10:49:22 +0800

    Add GitHub Actions CI

--- src/test/java/com/example/product_api/ProductApiApplicationTests.java
+++ src/test/java/com/example/product_api/ProductApiApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.product_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductApiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
